86e679265f7274b9c718afa124f9efa39b4a73c7
jflute <jflute@localhost>
2026-07-23 16:00:00 +0900

    1on1ふぉろー

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -55,11 +55,13 @@ public class Step01VariableTest extends PlainTestCase {
         //sea = land + "9";
         log(sea); // your answer? => mystic8:mai
     }
+    // TODO jflute 次回1on1で余談のし忘れをする (2026/07/23)
+
     //nullは反映されないと思っていたのでびっくり
     //そもそも異なる型同士で計算可能なのはなぜ？裏でto_string的なのが走ってる？
     //足し算の最初が Int でも正常に動作した(53行目) → 結果を入れる変数がStringなら全て文字列の結合として計算される？
     //54行目はダメで55行目は"89"になったので、どこかにStringが含まれてないと文字列の結合と見做されないのかも
-    // TODO nakamura [ふぉろー] まさしく裏で toString() が実行されています by jflute (2026/07/16)
+    // done nakamura [ふぉろー] まさしく裏で toString() が実行されています by jflute (2026/07/16)
     // +演算子は、Stringがあったら他の型はそこにひきずりこまれていきます。
     // "+" の意味がStringに優先されると言う感じですね。文字列連結するぞと。
 
@@ -67,29 +69,44 @@ public class Step01VariableTest extends PlainTestCase {
     //調べた結果：
     //int:早い、nullが入らない、プリテミティブ（←これもなに？？？）
     //Integer:nullが入る、クラス型（？）、メソッドが使える（？)、int を包むオブジェクト（？）
-    // TODO nakamura [読み物課題] Java Beginner's Hint - プリミティブ型とラッパー型 by jflute (2026/07/16)
+    // done nakamura [読み物課題] Java Beginner's Hint - プリミティブ型とラッパー型 by jflute (2026/07/16)
     // https://dbflute.seasar.org/ja/manual/topic/programming/java/beginners.html#primitivewrapper
+    // #1on1: 使い分け、ケースバイケースになる。nullを防ぐ手段、nullを許容する手段、として (2026/07/23)
 
     //クラス：設計図　こういう物を作れますよという定義　String,Integerなど Cの構造体に関数を持たせられるような感じ
     //オブジェクト：クラスから作られた実体 Integer land の landの方はオブジェクトへの参照を持ってる（Cのポインタみたいな感じ）
     //プリミティブ：値を直接持つ型 int double char など
     //メソッド：List　to_String　など
-    // TODO nakamura [ふぉろー] オブジェクトとインスタンスがニアリーイコールみたいな感じでよく使われます by jflute (2026/07/16)
+    // done nakamura [ふぉろー] オブジェクトとインスタンスがニアリーイコールみたいな感じでよく使われます by jflute (2026/07/16)
     // "Cの構造体に関数を持たせられるような感じ" という表現は本当に素晴らしい。
+    // #1on1: 広義と狭義のオブジェクトという言葉。実体を主張したい場合はインスタンス。 (2026/07/23)
+    // 一軒家の例。インスタンスの理解、mysticインスタンス、onemanインスタンス。
+    // インスタンス変数は、それぞれのインスタンスごとに独立した空間として存在している。
+    // 変数とインスタンスの関係性を把握しよう。test_variable_reassigned_basic()を見ながら解説。
+    // BigDecimalのインスタンス何個エクササイズやった。
+    // add()のコードリーディングもしました。
+    // プログラムをパッと見て、インスタンスをイメージできるようになろう。
 
     //to 久保さん:調べるごとにさらに気になることが出てきてしまい、発散しすぎてしまうように感じるのですが、どのように要点を絞ると良いでしょうか？
-    // TODO nakamura [へんじ] 好奇心旺盛なのは素晴らしい笑。確かに60分という制約もありますからね... by jflute (2026/07/16)
+    // done nakamura [へんじ] 好奇心旺盛なのは素晴らしい笑。確かに60分という制約もありますからね... by jflute (2026/07/16)
     // でも気になる時にインプットした方が効率良いというのもありますし、いま時間使った分、後のjavatryはサクッと進めるので、
     // 発散しすぎはそこまで気にしなくても良いとは思います。
     // さすがにキリがないって思ったら、自分用メモで、「#later 後でxxxを調べたい」とか目印を付けておくとか。
     // #later で検索すれば後で調べようと思ったことを一覧化できるので。
 
+    // #1on1: コード上の保留コメントのやり方のお話 (2026/07/23)
+    // TODO nakamura DBFlute な IntelliJ 補完テンプレート by jflute (2026/07/23)
+    // https://dbflute.seasar.org/ja/manual/topic/friends/intellij/index.html#dbflutelivetemplate
+
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_reassigned_basic() {
-        String sea = "mystic";
-        String land = "oneman";
-        sea = land;
-        land = land + "'s dreams";
+        String sea = "mystic"; // 1丁目1番地 => seaが指し示す
+        String land = "oneman"; // 2丁目2番地 => landが指し示す
+        sea = land; // seaの紙に書かれた1丁目1番地を消して、2丁目2番地に書き換える
+        // #1on1: ↑ここはアドレスをコピーしただけのイメージ。この時点でseaとlandは同じインスタンスを指し示す。 (2026/07/23)
+        // 質問: "mystic" はいつ削除されるのか？ → Javaの都合で、どこかのタイミングで削除される。
+        // ガベージコレクションのタイミング。参照されなくなったインスタンスを一気に削除。
+        land = land + "'s dreams"; // "'s dreams" は3丁目3番地、"oneman's dreams" は4丁目4番地
         log(sea); // your answer? => oneman
     }
 
@@ -100,15 +117,17 @@ public class Step01VariableTest extends PlainTestCase {
         sea = land;
         land++;
         log(sea); // your answer? => 415
+        // #1on1: プリミティヴ型の簡単な紹介 (2026/07/23)
+        // Pythonの数値が限界なし話 from なかむらさん
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_reassigned_BigDecimal() {
-        BigDecimal sea = new BigDecimal(94);
-        BigDecimal land = new BigDecimal(415);
+        BigDecimal sea = new BigDecimal(94); // 1
+        BigDecimal land = new BigDecimal(415); // 2
         sea = land;
-        sea = land.add(new BigDecimal(1));
-        sea.add(new BigDecimal(1));
+        sea = land.add(new BigDecimal(1)); // 3, 4
+        sea.add(new BigDecimal(1)); // 5, 6
         //sea = sea.add(new BigDecimal(1));
         log(sea); // your answer? => 417
     }
@@ -119,14 +138,28 @@ public class Step01VariableTest extends PlainTestCase {
     //10進数で計算する、誤差のない少数。immutable（変更不可） オブジェクト
     //"+"はプリミティブ型用なので、代わりに add を使う！ 中身は不変なので、新しい値が返される
     //99行目が加算になってないのは、addが中身を変えるメソッドじゃないからだ！！
-    // TODO nakamura [いいね] yes, そういうことです。 by jflute (2026/07/16)
+    // done nakamura [いいね] yes, そういうことです。 by jflute (2026/07/16)
 
     //100行目を試したところ、普通に加算された。あれ？immutableじゃないの？
     //調べた結果：seaという変数自体は可変 seaが参照しているBigDecimalオブジェクト本体は不変
     //オブジェクトの値が変更されたのではなく、seaが参照するオブジェクトが変わった
-    // TODO nakamura [いいね] yes, yes, そういうことです。 by jflute (2026/07/16)
+    // done nakamura [いいね] yes, yes, そういうことです。 by jflute (2026/07/16)
     // 自分自分のインスタンスの状態は変わってなく、seaという変数が指し示すインスタンスが差し変わっただけと。
     // この違いを理解するのはとても重要です。
+    // #1on1: immutableとは？ (不変な) (2026/07/23)
+    // o immutableなクラス(インスタンス) (デフォルト)
+    // o immutableな変数
+    //
+    // immutableのメリットは？
+    // o (デフォルト)安全であることが保証されている (後出し) by jflute
+    // o 変数のimmutableは読みやすくなるイメージ、変化を追わなくて良い、可読性 by なかむらさん
+    //   ↑インスタンスでも同じかなと by jflute
+    //   読み飛ばすこともできるケースも by jflute
+    // 「安全イコール可読性の典型例」
+    //
+    // immutableのデメリットは？
+    // o 
+    // TODO jflute 次回1on1でimmutableの続き (2026/07/23)
 
     //BigDecimalの疑問
     //"Big"ということは、普通のDecimalもある？
@@ -135,10 +168,10 @@ public class Step01VariableTest extends PlainTestCase {
     //→数万回呼ばれたり、数msのレスポンスを意識する場合は、適切にdoubleと使い分けることが必要
     //BigDecimal:重いけど正確。金額計算などに使う
     //double:早くてちょいちょい誤差が出る。ゲーム・物理演算などに使う
-    // TODO nakamura [ふぉろー] webサービスの実務だとほぼほぼ全部BigDecimalですね by jflute (2026/07/16)
+    // done nakamura [ふぉろー] webサービスの実務だとほぼほぼ全部BigDecimalですね by jflute (2026/07/16)
     // 変に誤差が出ても困るし、インフラの性能が格段に上がったので、BigDecimalのコストをあまり考えなくても良くなりました。
 
-    // TODO jflute 1on1にて、immutableのお話しじっくりフォロー予定。あとadd()のコードリーディングも (2026/07/16)
+    // done jflute 1on1にて、immutableのお話しじっくりフォロー予定。あとadd()のコードリーディングも (2026/07/16)
     // ↑くぼの備忘録とぅどぅなので、そのままにしておいて大丈夫です
 
     // ===================================================================================
